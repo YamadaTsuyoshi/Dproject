4bb6a6396c618a39fcfb6fa7c04c8cc559c8a915
YamadaTsuyoshi <zeal16300050@c-league.jp>
2018-03-23 14:38:58 +0900

    初期コミット

---
+++ FullTutorial001/GameSources/GameStage.cpp
@@ -0,0 +1,44 @@
+/*!
+@file GameStage.cpp
+@brief ゲームステージ実体
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+namespace basecross {
+
+	//--------------------------------------------------------------------------------------
+	//	ゲームステージクラス実体
+	//--------------------------------------------------------------------------------------
+
+
+
+	//ビューとライトの作成
+	void GameStage::CreateViewLight() {
+		auto PtrView = CreateView<SingleView>();
+		//ビューのカメラの設定
+		auto PtrCamera = PtrView->GetCamera();
+		PtrCamera->SetEye(Vec3(0.0f, 5.0f, -5.0f));
+		PtrCamera->SetAt(Vec3(0.0f, 0.0f, 0.0f));
+		//シングルライトの作成
+		auto PtrSingleLight = CreateLight<SingleLight>();
+		//ライトの設定
+		PtrSingleLight->GetLight().SetPositionToDirectional(-0.25f, 1.0f, -0.25f);
+	}
+
+
+
+
+	void GameStage::OnCreate() {
+		try {
+			//ビューとライトの作成
+			CreateViewLight();
+		}
+		catch (...) {
+			throw;
+		}
+	}
+
+}
+//end basecross
